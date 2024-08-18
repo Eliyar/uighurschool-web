8e8d8f87f0c7c14bdc823b57bebcbe1bfe21c52a
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-08-18 16:58:36 -0400

    Boilerplating code

---
+++ src/colors.ts
@@ -0,0 +1,22 @@
+// Application colours
+const Colors = {
+    TEXT: '#333333',
+    SECONDARY: '#636B74',
+    HINT: '#9e9e9e',
+    CONTENT_BACKGROUND: '#f5f5f5',
+    CONTENT_BACKGROUND_LIGHT: '#fafafa',
+    CONTROLLER_BACKGROUND: '#F0F4F8',
+    APP_BACKGROUND: '#f3f6f9',
+    BORDER_COLOR: '#e8e8e8',
+    BLUE: '#00a5f5',
+    BLUE_TINT: '#01a0ec',
+    RED: '#c7474a',
+    GREEN: '#82c64e',
+    YELLOW: '#ffc836',
+    ORANGE: '#ff9c2e',
+    PURPLE: '#7f39fb',
+    NAV: '#27364c',
+    NAV_DARK: '#1d2b3e',
+}
+
+export default Colors
