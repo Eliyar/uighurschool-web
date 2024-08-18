681ad951578b1819e88b5fca7c3e751c6c4a0f98
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-08-18 17:33:13 -0400

    Added logo component

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,21 @@
+import { styled, SxProps, Theme } from '@mui/material'
+
+interface Props {
+    className?: string
+    sx?: SxProps<Theme>
+}
+
+const Styles = styled('img')`
+    width: 60px;
+    height: auto;
+`
+
+export const Logo = ({ sx }: Props) => {
+    return (
+        <Styles
+            sx={{ ...sx }}
+            src={`${process.env.PUBLIC_URL}/assets/images/logo.svg`}
+            alt="Logo"
+        />
+    )
+}
