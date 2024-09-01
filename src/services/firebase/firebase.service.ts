b96d982ef1934e6dcdf2f0195f775192a1e4fab8
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-01 15:19:47 -0400

    Updated firebase integration

---
+++ src/services/firebase/firebase.service.ts
@@ -0,0 +1,36 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+import { setLogLevel as setFirestoreLogLevel } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+import { createFile } from './firestore/create-file'
+import { getFiles } from './firestore/get-files'
+import { uploadFile } from './storage/upload-file'
+
+setFirestoreLogLevel('debug')
+
+// TODO: move to .env file
+const firebaseConfig: FirebaseOptions = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
+export const firebaseDb = getDatabase(firebaseApp)
+export const firebaseStorage = getStorage(firebaseApp)
+
+export const firebaseService = {
+    db: {
+        getFiles,
+        createFile,
+    },
+    storage: {
+        uploadFile,
+    },
+}
