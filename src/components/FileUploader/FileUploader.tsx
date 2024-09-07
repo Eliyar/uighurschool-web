2cce31eb419635205b43f8ac859b9f0ef02f7733
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-07 13:42:04 -0400

    Refactored file-uploader component

--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -1,10 +1,7 @@
 import { Stack } from '@mui/material'
 import { useCallback, useState } from 'react'
 
-import { MAX_UPLOAD_PAGES } from '../../constants'
-import { firebaseService } from '../../services/firebase/firebase.service'
-import { FileModel } from '../../services/models/File.model'
-import { pdfService } from '../../services/pdfService'
+import { uploadFile } from '../../controllers/upload-file'
 import { BrowseFile } from '../common/BrowseFile'
 import { Button } from '../common/Button'
 import { TextField } from '../common/TextField'
@@ -13,53 +10,12 @@ export const FileUploader = () => {
     const [file, setFile] = useState<File | undefined>(undefined)
 
     const onUpload = useCallback(async () => {
-        if (!file) {
-            return
-        }
-
-        // TODO: check max file size
-
-        let fileModel: FileModel | undefined
         try {
-            const downloadUrl = await firebaseService.storage.uploadFile(file)
-            fileModel = FileModel.fromFile(file, downloadUrl)
+            await uploadFile(file)
+            // TODO: display toast message
         } catch (error) {
-            // TODO: display toast
-            console.error('Error splitting PDF:', error)
-        }
-
-        try {
-            const allPages = await pdfService.splitPdfPages(file)
-            const pagesToUpload = allPages.slice(0, MAX_UPLOAD_PAGES)
-
-            for (const page of pagesToUpload) {
-                try {
-                    const downloadUrl =
-                        await firebaseService.storage.uploadFile(page)
-                    const subFile: FileModel = FileModel.fromFile(
-                        page,
-                        downloadUrl
-                    )
-                    fileModel?.subFiles?.push(subFile)
-                } catch (error) {
-                    console.error('Error uploading page:', error)
-                }
-            }
-        } catch (error) {
-            console.error('Error splitting PDF:', error)
-        }
-
-        if (!fileModel) {
-            // TODO: display error toast
-            return
-        }
-
-        try {
-            await firebaseService.db.createFile(fileModel)
-
-            // TODO: display success toast
-        } catch (error) {
-            console.error('Error creating file:', error)
+            // TODO: display toast message
+            console.error(error)
         }
     }, [file])
 
---
+++ src/controllers/upload-file.ts
@@ -0,0 +1,57 @@
+import { MAX_UPLOAD_PAGES } from '../constants'
+import { firebaseService } from '../services/firebase/firebase.service'
+import { FileModel } from '../services/models/File.model'
+import { pdfService } from '../services/pdfService'
+
+export const uploadFile = async (
+    file: File | undefined
+): Promise<FileModel> => {
+    if (!file) {
+        return Promise.reject('No file provided')
+    }
+
+    // TODO: check max file size
+
+    // Upload base file
+    let fileModel: FileModel | undefined
+    try {
+        const downloadUrl = await firebaseService.storage.uploadFile(file)
+        fileModel = FileModel.fromFile(file, downloadUrl)
+    } catch (error) {
+        // TODO: display toast
+        console.error('Error splitting PDF:', error)
+        return Promise.reject(error)
+    }
+
+    // Split file into separate pages and upload them
+    try {
+        const allPages = await pdfService.splitPdfPages(file)
+        const pagesToUpload = allPages.slice(0, MAX_UPLOAD_PAGES)
+
+        for (const page of pagesToUpload) {
+            try {
+                const downloadUrl = await firebaseService.storage.uploadFile(
+                    page
+                )
+                const subFile: FileModel = FileModel.fromFile(page, downloadUrl)
+                fileModel.subFiles?.push(subFile)
+            } catch (error) {
+                console.error('Error uploading page:', error)
+                return Promise.reject(error)
+            }
+        }
+    } catch (error) {
+        console.error('Error splitting PDF:', error)
+        return Promise.reject(error)
+    }
+
+    // Create file in database
+    try {
+        await firebaseService.db.createFile(fileModel)
+    } catch (error) {
+        console.error('Error creating file:', error)
+        return Promise.reject(error)
+    }
+
+    return Promise.resolve(fileModel)
+}
