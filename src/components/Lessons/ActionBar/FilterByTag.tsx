6e09bd988a2bb943babb1523aa73c93e92a22c31
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-08 16:48:01 -0400

    Updated the design of filter-by-tag component

--- src/components/Lessons/ActionBar/FilterByTag.tsx
+++ src/components/Lessons/ActionBar/FilterByTag.tsx
@@ -1,20 +1,48 @@
-import { ToggleButton, ToggleButtonGroup } from '@mui/material'
+import {
+    styled,
+    ToggleButton,
+    toggleButtonClasses,
+    ToggleButtonGroup,
+    toggleButtonGroupClasses,
+} from '@mui/material'
 
+import Colors from '../../../colors'
 import { useLocalContext } from '../hooks/useLocalContext'
+
+const Styles = styled(ToggleButtonGroup)`
+    &.${toggleButtonGroupClasses.root} {
+        padding: 2px;
+        background-color: ${Colors.CONTROLLER_BACKGROUND};
+    }
+
+    .${toggleButtonClasses.root} {
+        padding-left: 12px;
+        padding-right: 12px;
+        background-color: transparent;
+        border: 0;
+        text-transform: none;
+        border-radius: 6px;
+    }
+
+    .${toggleButtonClasses.selected} {
+        background-color: white;
+        border: 1px solid ${Colors.BORDER_COLOR};
+    }
+`
 
 export const FilterByTag = () => {
     const { tag, setTag } = useLocalContext().filters
 
     return (
-        <ToggleButtonGroup
+        <Styles
             exclusive
             size="small"
             value={tag}
-            onChange={(_, value) => setTag(value)}
+            onChange={(_, value) => setTag(value ?? 'all')}
         >
             <ToggleButton value="all">All</ToggleButton>
             <ToggleButton value="lesson">Lessons</ToggleButton>
             <ToggleButton value="story">Stories</ToggleButton>
-        </ToggleButtonGroup>
+        </Styles>
     )
 }
