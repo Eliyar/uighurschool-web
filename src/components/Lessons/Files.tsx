b5dfa2aa6a32579ce8cc83f8d50077c730a90c3e
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-14 17:37:44 -0400

    Updated empty state for files component

--- src/components/Lessons/Files.tsx
+++ src/components/Lessons/Files.tsx
@@ -1,19 +1,42 @@
 import { InfoOutlined } from '@mui/icons-material'
-import { Alert, Grid } from '@mui/material'
+import { Alert, Box, Grid, Stack, Typography } from '@mui/material'
 
+import { Button } from '../common/Button'
 import { FileItem } from '../common/FileItem/FileItem'
 import { useLocalContext } from './hooks/useLocalContext'
 
 export const Files = () => {
     const { files } = useLocalContext().files
-    const { searchTerm } = useLocalContext().filters
+    const { searchTerm, setSearchTerm } = useLocalContext().filters
 
     if (!files.length) {
         return (
             <Alert color="warning" icon={<InfoOutlined />}>
-                {searchTerm
-                    ? `No lessons found for "${searchTerm}"`
-                    : 'No lessons exist'}
+                {searchTerm ? (
+                    <Stack spacing={2}>
+                        <Box>
+                            No lessons found for &quot;
+                            <Typography
+                                variant="body2"
+                                fontWeight={500}
+                                component="span"
+                            >
+                                {searchTerm}
+                            </Typography>
+                            &quot;
+                        </Box>
+                        <Button
+                            variant="outlined"
+                            color="info"
+                            label="Clear Search"
+                            onClick={() => setSearchTerm('')}
+                            size="small"
+                            sx={{ width: 'fit-content' }}
+                        />
+                    </Stack>
+                ) : (
+                    'No lessons exist'
+                )}
             </Alert>
         )
     }
