bc92c707cc6d0dd1c28d2fa758be5db34dfa327f
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-22 16:50:48 -0400

    Updated send-lessons action to save lesson

--- src/controllers/send-lessons.ts
+++ src/controllers/send-lessons.ts
@@ -1,14 +1,18 @@
+import { createLesson } from '../services/firebase/db/lessons/create'
 import { utilsService } from '../services/firebase/utils.service'
 import { httpService } from '../services/http/http.service'
 import { FileModel } from '../services/models/File.model'
+import { Lesson } from '../services/models/Lesson.model'
 import { Student } from '../services/models/Student.model'
+import { LessonAdded } from '../services/store/actions'
 
 export const sendLessons = async (payload: {
     students: Student[]
     subject: string
     message: string
     files: FileModel[]
-}) => {
+    classId?: string
+}): Promise<Lesson> => {
     const uniqueStudents = payload.students.filter(
         (student, index, self) =>
             index === self.findIndex((t) => t.email === student.email)
@@ -21,10 +25,22 @@ export const sendLessons = async (payload: {
         )
     )
 
-    return httpService.sendEmail({
-        to,
-        subject: payload.subject,
-        body: payload.message ?? '',
-        attachments,
-    })
+    return httpService
+        .sendEmail({
+            to,
+            subject: payload.subject,
+            body: payload.message ?? '',
+            attachments,
+        })
+        .then(async () => {
+            const lesson = new Lesson(
+                payload.subject,
+                payload.classId,
+                payload.files?.map((file) => file.id) ?? []
+            )
+            return createLesson(lesson).then(() => {
+                LessonAdded.dispatch(lesson)
+                return lesson
+            })
+        })
 }
