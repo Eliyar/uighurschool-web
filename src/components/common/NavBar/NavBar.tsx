6f702e4a5603e3c729845847c5ec393e54739af4
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-22 18:17:10 -0400

    Updated nav-bar component to allow closing all nav-items

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -4,7 +4,10 @@ import { useParams } from 'react-router'
 import { NavLink } from 'react-router-dom'
 
 import Colors from '../../../colors'
-import { deleteNavItem } from '../../../controllers/delete-navitem'
+import {
+    deleteAllNavItems,
+    deleteNavItem,
+} from '../../../controllers/delete-navitem'
 import { useNavItems } from '../../../hooks/useNavItems'
 import { useRoute } from '../../../hooks/useRoute'
 import { Button } from '../Button'
@@ -39,51 +42,66 @@ export const NavBar = () => {
 
 const NavItems = () => {
     const { id } = useParams<{ id: string }>()
-    const { navMain, navViewUrl } = useRoute()
+    const { navLessons, navViewUrl } = useRoute()
     const { navItems } = useNavItems()
 
     return (
-        <Tabs
-            value={false}
-            variant="scrollable"
-            scrollButtons="auto"
-            sx={{ flex: '1 1 100%', width: '100px', minWidth: '100px' }}
-        >
-            {navItems.map((navItem) => {
-                const isActive = navItem.id === id
+        <>
+            <Tabs
+                value={false}
+                variant="scrollable"
+                scrollButtons="auto"
+                sx={{ flex: '1 1 100%', width: '100px', minWidth: '100px' }}
+            >
+                {navItems.map((navItem) => {
+                    const isActive = navItem.id === id
 
-                return (
-                    <Tab
-                        key={navItem.id}
-                        label={
-                            <Button
-                                key={navItem.id}
-                                variant={isActive ? 'contained' : 'outlined'}
-                                label={navItem.name}
-                                onClick={() => {
-                                    navViewUrl(navItem.id)
-                                }}
-                                endIcon={
-                                    <IconButton
-                                        onClick={(event) => {
-                                            event.stopPropagation()
-                                            deleteNavItem(navItem)
-                                            if (isActive) {
-                                                navMain()
-                                            }
-                                        }}
-                                        sx={{ ml: 1 }}
-                                    >
-                                        <Close fontSize="small" />
-                                    </IconButton>
-                                }
-                                sx={{ pr: 1, mr: 1, whiteSpace: 'nowrap' }}
-                            />
-                        }
-                        sx={{ p: 0 }}
-                    />
-                )
-            })}
-        </Tabs>
+                    return (
+                        <Tab
+                            key={navItem.id}
+                            label={
+                                <Button
+                                    key={navItem.id}
+                                    variant={
+                                        isActive ? 'contained' : 'outlined'
+                                    }
+                                    label={navItem.name}
+                                    onClick={() => {
+                                        navViewUrl(navItem.id)
+                                    }}
+                                    endIcon={
+                                        <IconButton
+                                            onClick={(event) => {
+                                                event.stopPropagation()
+                                                deleteNavItem(navItem)
+                                                if (isActive) {
+                                                    navLessons()
+                                                }
+                                            }}
+                                            sx={{ ml: 1 }}
+                                        >
+                                            <Close fontSize="small" />
+                                        </IconButton>
+                                    }
+                                    sx={{ pr: 1, mr: 1, whiteSpace: 'nowrap' }}
+                                />
+                            }
+                            sx={{ p: 0 }}
+                        />
+                    )
+                })}
+            </Tabs>
+
+            {navItems.length > 0 && (
+                <IconButton
+                    onClick={() => {
+                        deleteAllNavItems()
+                        navLessons()
+                    }}
+                >
+                    <Close />
+                </IconButton>
+            )}
+        </>
     )
 }
