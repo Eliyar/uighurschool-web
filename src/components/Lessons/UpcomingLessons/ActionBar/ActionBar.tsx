7f4946153b4668a391c10606f3f603330cefedeb
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-09-22 18:55:39 -0400

    Added navigation to classes route

--- src/components/Lessons/UpcomingLessons/ActionBar/ActionBar.tsx
+++ src/components/Lessons/UpcomingLessons/ActionBar/ActionBar.tsx
@@ -1,8 +1,13 @@
+import { EmailOutlined } from '@mui/icons-material'
 import { Stack, Typography } from '@mui/material'
 
+import { useRoute } from '../../../../hooks/useRoute'
+import { Button } from '../../../common/Button'
 import { SendLessonsDialogTrigger } from '../../../common/SendLessonsDialog/SendLessonsDialogTrigger'
 
 export const ActionBar = () => {
+    const { navClasses } = useRoute()
+
     return (
         <Stack
             direction="row"
@@ -13,7 +18,17 @@ export const ActionBar = () => {
                 <Typography variant="h6">Lessons</Typography>
             </Stack>
 
-            <SendLessonsDialogTrigger />
+            <Stack direction="row" spacing={2}>
+                <Button
+                    variant="outlined"
+                    label="Manage Classes"
+                    size="small"
+                    startIcon={<EmailOutlined />}
+                    onClick={() => navClasses()}
+                    sx={{ px: 3 }}
+                />
+                <SendLessonsDialogTrigger />
+            </Stack>
         </Stack>
     )
 }
