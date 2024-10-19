b26c89f01c39309c8265da33c8c81b77dcf06bc0
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-10-19 15:49:01 -0400

    Downgraded pdfjs-dist version

--- src/components/common/PDFViewer.tsx
+++ src/components/common/PDFViewer.tsx
@@ -1,42 +1,32 @@
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
-import { Viewer } from '@react-pdf-viewer/core'
+import { Viewer, Worker, ZoomEvent } from '@react-pdf-viewer/core'
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-import { pdfjs } from 'react-pdf'
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url
-).toString()
 
 interface Props {
     url: string
 }
 
+const workerUrl =
+    'https://cdn.jsdelivr.net/npm/pdfjs-dist@3.0.279/build/pdf.worker.min.js'
+
 export const PDFViewer = ({ url }: Props) => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin()
 
-    // const handleZoom = (props: any) => {
-    //     // props?.viewer?.zoom('fitWidth')
-    // }
+    const handleZoom = (event: ZoomEvent) => {
+        console.log('event:', event)
+    }
 
     return (
         <div style={{ height: '100vh', width: '100%' }}>
-            {/* <Worker
-                // workerUrl={
-                //     // 'https://unpkg.com/pdfjs-dist@2.11.338/build/pdf.worker.min.js'
-                //     '../node_modules/pdfjs-dist/build/pdf.worker.min.mjs'
-                // }
-            > */}
-            <Viewer
-                fileUrl={url}
-                plugins={[defaultLayoutPluginInstance]}
-                // onZoom={handleZoom} // Fit PDF to page width when loaded
-            />
-            {/* </Worker> */}
-
-            {/* <Document file={url} /> */}
+            <Worker workerUrl={workerUrl}>
+                <Viewer
+                    fileUrl={url}
+                    plugins={[defaultLayoutPluginInstance]}
+                    onZoom={handleZoom}
+                />
+            </Worker>
         </div>
     )
 }
