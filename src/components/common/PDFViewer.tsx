5e6930b097eedf63fdcc6bc3d41b8251f978d9fd
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-10-19 17:25:00 -0400

    Customized pdf-viewer component

--- src/components/common/PDFViewer.tsx
+++ src/components/common/PDFViewer.tsx
@@ -56,7 +56,6 @@ const ZoomControlsStyles = styled(Stack)`
     border-radius: 6px;
     box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
     font-weight: bold;
-    zoom: 1.2;
 `
 
 const ZoomControls = ({
